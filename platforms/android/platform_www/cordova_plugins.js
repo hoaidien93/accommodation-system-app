4490527e0cf7b0638b57f74b74f65fe371b52f6f
diendh <diendh@vng.com.vn>
2020-07-09 02:04:18 +0700

    Add Android login google

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -9,6 +9,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       ]
     },
     {
+      "id": "cordova-plugin-firebase-authentication.FirebaseAuthentication",
+      "file": "plugins/cordova-plugin-firebase-authentication/www/FirebaseAuthentication.js",
+      "pluginId": "cordova-plugin-firebase-authentication",
+      "merges": [
+        "cordova.plugins.firebase.auth"
+      ]
+    },
+    {
       "id": "cordova-plugin-firebase-messaging.FirebaseMessaging",
       "file": "plugins/cordova-plugin-firebase-messaging/www/FirebaseMessaging.js",
       "pluginId": "cordova-plugin-firebase-messaging",
@@ -17,35 +25,11 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       ]
     },
     {
-      "id": "cordova-plugin-buildinfo.BuildInfo",
-      "file": "plugins/cordova-plugin-buildinfo/www/buildinfo.js",
-      "pluginId": "cordova-plugin-buildinfo",
+      "id": "cordova-plugin-googleplus.GooglePlus",
+      "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
+      "pluginId": "cordova-plugin-googleplus",
       "clobbers": [
-        "BuildInfo"
-      ]
-    },
-    {
-      "id": "cordova-universal-links-plugin.universalLinks",
-      "file": "plugins/cordova-universal-links-plugin/www/universal_links.js",
-      "pluginId": "cordova-universal-links-plugin",
-      "clobbers": [
-        "universalLinks"
-      ]
-    },
-    {
-      "id": "cordova-plugin-browsertab.BrowserTab",
-      "file": "plugins/cordova-plugin-browsertab/www/browsertab.js",
-      "pluginId": "cordova-plugin-browsertab",
-      "clobbers": [
-        "cordova.plugins.browsertab"
-      ]
-    },
-    {
-      "id": "cordova-plugin-inappbrowser.inappbrowser",
-      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-      "pluginId": "cordova-plugin-inappbrowser",
-      "clobbers": [
-        "cordova.InAppBrowser.open"
+        "window.plugins.googleplus"
       ]
     }
   ];
@@ -53,11 +37,9 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-support-android-plugin": "1.0.2",
     "cordova-support-google-services": "1.4.0",
     "cordova-plugin-firebase-analytics": "4.3.0",
+    "cordova-plugin-firebase-authentication": "3.3.0",
     "cordova-plugin-firebase-messaging": "4.4.1",
-    "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-buildinfo": "4.0.0",
-    "cordova-universal-links-plugin": "1.2.1",
-    "cordova-plugin-browsertab": "0.2.0",
-    "cordova-plugin-inappbrowser": "4.0.0"
+    "cordova-plugin-googleplus": "8.5.0",
+    "cordova-plugin-whitelist": "1.3.4"
   };
 });
